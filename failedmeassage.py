296731a55a7210f615150c5b8e7b227c128b9c4f
shaileshchoudhary <43171769+shaileshchoudhary@users.noreply.github.com>
2020-07-15 11:09:26 +0530

    Update failedmeassage.py

--- failedmeassage.py
+++ failedmeassage.py
@@ -8,6 +8,6 @@ s.login("shaileshchoudhary1999@gmail.com", "password")
 # message to be sent 
 message = "Hey Developer,Website is not working. please check the code and push again"
 # sending the mail 
-s.sendmail("shaileshchoudhary1999@gmail.com", "shaileshchoudhary1999@gmail.com", message) 
+s.sendmail("sender", "reciever", message) 
 # terminating the session 
 s.quit()
